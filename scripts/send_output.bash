eb82610999f147f8c41b9f8a5f4ffd69f8f7e4f2
Shalom Yiblet <shalom.yiblet@gmail.com>
2019-03-18 22:00:40 -0400

    changed location of where the logs are sent

--- scripts/send_output.bash
+++ scripts/send_output.bash
@@ -7,6 +7,6 @@ while read -r _; do
     cur=$(date +"%s")
     if [[ $((last + 30)) -le $cur ]]; then
         last=$cur
-        gsutil cp -r "${location:-/content/output.txt}" "gs://yiblet_research/${run_type:-default}_output.txt"
+        gsutil cp -r "${location:-/content/output.txt}" "gs://yiblet_research/logs/${run_type:-default}.txt"
     fi
 done
